7a834e73758efc3a60b5677b2c99f284ad1af1df
Kashaedov Vitaliy <Vitalickashaedov@mail.ru>
2022-07-11 18:04:11 +0300

    Zadacha25

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите число A: ");
+int number1 = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int number2 = Convert.ToInt32 (Console.ReadLine());
+int itog = 1;
+void  Zadacha25 (int number1, int number2, int itog )
+{
+    for ( int i = 0; i < number2; i = i + 1 )
+    {
+    itog = number1 * itog;
+
+    }
+
+
+Console.WriteLine($"Число {number1} в степени {number2} = {itog}  ");
+}
+Zadacha25 (number1, number2, itog );
